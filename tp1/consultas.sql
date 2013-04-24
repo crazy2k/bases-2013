2dd417d3897a9ed6ca9d7aaa09a5780fd0c8a40b
Nadia <heredianadia@gmail.com>
2013-04-24 18:28:19 -0300

    Una parte de la consulta que borra reservas, falta lo del precio

--- tp1/consultas.sql
+++ tp1/consultas.sql
@@ -63,17 +63,58 @@ begin
 	  		or s.fecha_llegada <= fecha_salida))
 	)
 	begin
-	  raiserror ('Se superpone la reserva con una ya realizada.', 16, 1);
+	  raiserror ('Se superpone la reserva con una ya realizada.', 5, 1);
 	  rollback transaction;
 	  return
 	end;
-end
+end;
 
 
--- Consulta 3 (NO terminada)
+-- Consulta 3
 
-CREATE PROCEDURE reservas_superpuestas()
-LANGUAGE SQL
-BEGIN
+delete from reserva r1
+where
+exists
+(
+	select *
+	from reserva r2, tiene t, servicio s, hace h, 
+			usuario u, vuelvo v
+	where t.nro_reserva = r2.nro_reserva
+	and t.id_servicio = s.id_servicio
+	and h.nro_reserva = r2.nro_reserva
+	and h.id_user = u.id_user
+	and s.asociadoA = v.nro_vuelo	
+	/* Hasta aca tenemos las reservas del usuario */
+	
+	/* Iguales fechas */
+	and s.fecha_salida = 
+		(select s2.fecha_salida
+		from servicio s2, tiene t2	
+		where s2.id_servicio = t2.id_servicio 
+		and t2.nro_reserva = r1.nro_reserva)
 
-END
+	and s.fecha_llega = 
+		(select s2.fecha_llegada
+		from servicio s2, tiene t2	
+		where s2.id_servicio = t2.id_servicio 
+		and t2.nro_reserva = r1.nro_reserva)
+
+	/* Iguales aeropuerto */
+	and v.saleDe = 
+		(select v2.saleDe
+		from servicio s2, tiene t2, vuelvo v2	
+		where s2.id_servicio = t2.id_servicio 
+		and t2.nro_reserva = r1.nro_reserva
+		and s2.asociadoA = v2.nro_vuelo)
+
+	and v.llegaA = 
+		(select v2.llegaA
+		from servicio s2, tiene t2, vuelvo v2	
+		where s2.id_servicio = t2.id_servicio 
+		and t2.nro_reserva = r1.nro_reserva
+		and s2.asociadoA = v2.nro_vuelo)
+
+	/* Menor precio */
+
+	#and precio < precio2
+)
