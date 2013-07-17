7465ec61483cf1b5c0f02202bdb3cb3633343b94
Vanesa Stricker <vstricker@monits.com>
2013-07-17 15:56:05 -0300

    Modifico la consulta 1, ahora en vez de chequear por todos los paises existentes en toda la besa de datos, chequea los paises que son origen o destino de algu vuelo

--- tp1/consultas/1-Pasajeros.sql
+++ tp1/consultas/1-Pasajeros.sql
@@ -1,25 +1,26 @@
-select dp.nombre, dp.apellido, id_user
-from usuario u, datospersonales dp
+select dp.nombre, dp.apellido, u.id_user
+from Usuario u, DatosPersonales dp
 where not exists 
 (
-    select *
-    from pais p
-    where not exists 
+    select p.id_pais, p.nombre
+		from Vuelo v1, Aeropuerto a1, Ciudad c1, Pais p
+		where (v1.a_salida = a1.id_a or v1.a_llegada = a1.id_a)
+		and a1.id_ciudad = c1.id_ciudad
+		and c1.id_pais = p.id_pais
+		and not exists
     (
         select *
-        from hace h, reserva r, tiene t, servicio s, vuelo v,
-            aeropuerto a, ciudad c
+        from Hace h, Reserva r, Tiene t, Servicio s, Vuelo v, Aeropuerto a, Ciudad c
         where u.id_user = h.id_user 
-        and h.nro_reserva = r.nro_reserva 
-        and r.nro_reserva = t.nro_reserva 
-        and t.id_servicio = s.id_servicio
-        and s.nro_vuelo = v.nro_vuelo 
-        and (v.llega_a = a.id_a or v.sale_de = a.id_a) 
+        and h.id_reserva = r.id_reserva 
+        and r.id_reserva = t.id_res 
+        and t.id_serv = s.id_servicio
+        and s.id_vuelo = v.id_vuelo 
+        and (v.a_llegada = a.id_a or v.a_salida = a.id_a) 
         and a.id_ciudad = c.id_ciudad 
-        and c.id_pais = p.id_pais 
-        and ((s.fecha >= date_sub(now(), interval 5 year)) 
-            or (s.fecha >= date_sub(now(), interval 5 year)))  
+        and c.id_pais = p.id_pais
+        and ((s.salida >= date_sub(now(), interval 5 year)) 
+            or (s.llegada >= date_sub(now(), interval 5 year)))  
     )
 )
 and u.id_user = dp.id_user
-
