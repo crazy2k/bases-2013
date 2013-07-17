a63f592f89d6afdd45a59e107881fd91b0236003
Vanesa Stricker <vstricker@monits.com>
2013-07-17 16:12:02 -0300

    Arreglo la sintaxis de la consulta 3. De ahi a que haga lo que tiene que hacer...

--- tp1/consultas/3-ReservasSuperpuestas.sql
+++ tp1/consultas/3-ReservasSuperpuestas.sql
@@ -1,4 +1,5 @@
 drop trigger chequear_reservas_superpuestas;
+delimiter //
 create trigger chequear_reservas_superpuestas
 BEFORE INSERT ON Reserva
 FOR EACH ROW
@@ -15,34 +16,31 @@ begin
 					where r2.id_reserva <> NEW.id_reserva
 						and h2.id_reserva = r2.id_reserva
 						and h2.id_user = h1.id_user
-						and r2.id_reserva = t2.id_reserva
-						and t2.id_servicio = s2.id_servicio)
-/*Hasta aca tengo todas las reservas de un usuario son en distinta fecha*/
-/*				EXCEPT
-				(select s3.salida
-				from reserva r3, hace h3, tiene t3, servicio s3, usuario u3, vuelo v3
-				where r3.id_reserva = h3.id_reserva
-					and r3.id_reserva = t3.id_reserva
-					and u3.id_user = h3.id_user
-					and t3.id_servicio = s3.id_servicio
-					and s3.id_vuelo = v3.id_vuelo
-					and s3.salida IN (
-											select s4.fecha_salida
-											from servicio s4, hace h4, tiene t4, reserva r4, vuelo v4
-											where r4.id_reserva <> r3.id_reserva
-												and h4.id_reserva = r4.id_reserva
-												and h4.id_user = h3.id_user
-												and r4.id_reserva = t4.id_reserva
-												and t4.id_servicio = s4.id_servicio
-												and s4.id_vuelo = v4.id_vuelo
-												and v4.a_salida = v3.a_salida
-												and v4.a_llegada = v4.a_llegada
-												and data_sub(s4.fecha_salida, now()) > 7)*/
-				-- group by r3.id_reserva
-			-- 	having count (*) < 3
-			--	)
-			)
-	) THEN
+						and r2.id_reserva = t2.id_res
+						and t2.id_serv = s2.id_servicio
+						and s2.salida NOT IN (
+							(select s3.salida
+							from reserva r3, hace h3, tiene t3, servicio s3, usuario u3, vuelo v3
+							where r3.id_reserva = h3.id_reserva
+								and r3.id_reserva = t3.id_res
+								and u3.id_user = h3.id_user
+								and t3.id_serv = s3.id_servicio
+								and s3.id_vuelo = v3.id_vuelo
+								and s3.salida IN (
+														select s4.fecha_salida
+														from servicio s4, hace h4, tiene t4, reserva r4, vuelo v4
+														where r4.id_reserva <> r3.id_reserva
+															and h4.id_reserva = r4.id_reserva
+															and h4.id_user = h3.id_user
+															and r4.id_reserva = t4.id_res
+															and t4.id_serv = s4.id_servicio
+															and s4.id_vuelo = v4.id_vuelo
+															and v4.a_salida = v3.a_salida
+															and v4.a_llegada = v4.a_llegada
+															and data_sub(s4.fecha_salida, now()) > 7)
+							) 
+						)
+	))) THEN
 					SIGNAL SQLSTATE '45000'
 						SET MESSAGE_TEXT = 'No se puede insertar la reserva';
 				END IF;
