4d22eb5c07df287452ec3e5157e0f09e3d893cc6
Vanesa Stricker <vstricker@monits.com>
2013-07-17 17:44:55 -0300

    Pequenio arreglo en la consulta 1: ahora chequeamos que el estado de la fecha sea realizado (estado 3)

--- tp1/consultas/1-Pasajeros.sql
+++ tp1/consultas/1-Pasajeros.sql
@@ -9,18 +9,19 @@ where not exists
 		and c1.id_pais = p.id_pais
 		and not exists
     (
-        select *
-        from Hace h, Reserva r, Tiene t, Servicio s, Vuelo v, Aeropuerto a, Ciudad c
-        where u.id_user = h.id_user 
-        and h.id_reserva = r.id_reserva 
-        and r.id_reserva = t.id_res 
-        and t.id_serv = s.id_servicio
-        and s.id_vuelo = v.id_vuelo 
-        and (v.a_llegada = a.id_a or v.a_salida = a.id_a) 
-        and a.id_ciudad = c.id_ciudad 
-        and c.id_pais = p.id_pais
-        and ((s.salida >= date_sub(now(), interval 5 year)) 
-            or (s.llegada >= date_sub(now(), interval 5 year)))  
+			select *
+			from Hace h, Reserva r, Tiene t, Servicio s, Vuelo v, Aeropuerto a, Ciudad c, Estado e
+			where u.id_user = h.id_user 
+			and h.id_reserva = r.id_reserva 
+			and r.id_reserva = t.id_res 
+			and t.id_serv = s.id_servicio
+			and s.id_vuelo = v.id_vuelo 
+			and (v.a_llegada = a.id_a or v.a_salida = a.id_a) 
+			and a.id_ciudad = c.id_ciudad 
+			and c.id_pais = p.id_pais
+			and r.id_estado = 3
+			and ((s.salida >= date_sub(now(), interval 5 year)) 
+					or (s.llegada >= date_sub(now(), interval 5 year)))  
     )
 )
 and u.id_user = dp.id_user
