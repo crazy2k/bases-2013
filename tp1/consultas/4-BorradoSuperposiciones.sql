773e3b24b65c86744698d0a186f7e55df429bb3c
Vanesa Stricker <vstricker@monits.com>
2013-07-17 16:24:43 -0300

    Algunos arreglos a la consulta 4, fata verificar que haga lo que tiene que hacer

--- tp1/consultas/4-BorradoSuperposiciones.sql
+++ tp1/consultas/4-BorradoSuperposiciones.sql
@@ -1,54 +1,41 @@
-delete from reserva r1
-where
-exists
-(
-	select *
-	from reserva r2, tiene t, servicio s, hace h, 
-			usuario u, vuelvo v, brinda b
+/*Cambiar el select* por delete, es solo para probar*/
+select * 
+from reserva r1
+where exists(
+	select *, MIN(b.precio)
+	from reserva r2, tiene t, servicio s, hace h, usuario u, vuelo v, brinda b
 	where t.nro_reserva = r2.nro_reserva
 	and t.id_servicio = s.id_servicio
 	and h.nro_reserva = r2.nro_reserva
 	and h.id_user = u.id_user
-	and s.asociadoA = v.nro_vuel
-	and b.id_servicio = s.id_servicio
-	/* Hasta aca tenemos las reservas del usuario */
+	and s.id_vuelo = v.id_vuelo	
+/* Hasta aca tenemos las reservas del usuario */
 	
-	/* Iguales fechas */
-	and s.fecha_salida = 
-		(select s2.fecha_salida
+/* Iguales fechas */
+	and s.fecha_salida IN (
+		select s2.fecha_salida
 		from servicio s2, tiene t2	
 		where s2.id_servicio = t2.id_servicio 
 		and t2.nro_reserva = r1.nro_reserva)
 
-	and s.fecha_llega = 
-		(select s2.fecha_llegada
-		from servicio s2, tiene t2	
-		where s2.id_servicio = t2.id_servicio 
-		and t2.nro_reserva = r1.nro_reserva)
+	and s.fecha_llegada IN ( 
+		select s3.fecha_llegada
+		from servicio s3, tiene t3	
+		where s3.id_servicio = t3.id_servicio 
+		and t3.nro_reserva = r1.nro_reserva)
 
-	/* Iguales aeropuerto */
-	and v.saleDe = 
-		(select v2.saleDe
-		from servicio s2, tiene t2, vuelvo v2	
-		where s2.id_servicio = t2.id_servicio 
-		and t2.nro_reserva = r1.nro_reserva
-		and s2.nro_vuelo = v2.nro_vuelo)
+/* Iguales aeropuerto */
+	and v.aeropuerto_salida IN ( 
+		select v4.aeropuerto_salida
+		from servicio s4, tiene t4, vuelvo v4
+		where s4.id_servicio = t4.id_servicio 
+		and t4.nro_reserva = r1.nro_reserva
+		and s4.id_vuelo = v4.id_vuelo)
 
-	and v.llegaA = 
-		(select v2.llegaA
-		from servicio s2, tiene t2, vuelvo v2	
-		where s2.id_servicio = t2.id_servicio 
-		and t2.nro_reserva = r1.nro_reserva
-		and s2.nro_vuelo = v2.nro_vuelo)
-
-	/* Fecha partida dentro de los proximos 7 dias */
-	and data_sub(s.fecha_salida, now()) < 7
-
-	/* Menor precio */
-	and sum(b.precio) < sum(
-		select b2.precio
-		from servicio s2, brinda b2, tiene t2
-		where s2.id_servicio = t2.id_servicio
-		and t2.nro_reserva = r1.nro_reserva
-		and b2.id_servicio = s2.id_servicio)
+	and v.aeropuerto_llegada IN ( 
+		select v5.aeropuerto_llegada
+		from servicio s5, tiene t5, vuelvo v5
+		where s5.id_servicio = t5.id_servicio 
+		and t5.nro_reserva = r1.nro_reserva
+		and s5.id_vuelo = v5.id_vuelo)		
 )
