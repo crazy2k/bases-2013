a2b17011d79d978e8028e704eab7efbbb6451a50
Vanesa Stricker <vstricker@monits.com>
2013-07-17 16:32:30 -0300

    Mas arreglos de sintaxis de la consulta 4, ahora devuelve cosas, pero creo que no devuelve lo que deberia

--- tp1/consultas/4-BorradoSuperposiciones.sql
+++ tp1/consultas/4-BorradoSuperposiciones.sql
@@ -1,41 +1,41 @@
 /*Cambiar el select* por delete, es solo para probar*/
 select * 
-from reserva r1
+from Reserva r1
 where exists(
 	select *, MIN(b.precio)
-	from reserva r2, tiene t, servicio s, hace h, usuario u, vuelo v, brinda b
-	where t.nro_reserva = r2.nro_reserva
-	and t.id_servicio = s.id_servicio
-	and h.nro_reserva = r2.nro_reserva
+	from Reserva r2, Tiene t, Servicio s, Hace h, Usuario u, Vuelo v, Brinda b
+	where t.id_res = r2.id_reserva
+	and t.id_serv = s.id_servicio
+	and h.id_reserva = r2.id_reserva
 	and h.id_user = u.id_user
 	and s.id_vuelo = v.id_vuelo	
 /* Hasta aca tenemos las reservas del usuario */
 	
 /* Iguales fechas */
-	and s.fecha_salida IN (
-		select s2.fecha_salida
-		from servicio s2, tiene t2	
-		where s2.id_servicio = t2.id_servicio 
-		and t2.nro_reserva = r1.nro_reserva)
+	and s.salida IN (
+		select s2.salida
+		from Servicio s2, Tiene t2	
+		where s2.id_servicio = t2.id_serv 
+		and t2.id_res = r1.id_reserva)
 
-	and s.fecha_llegada IN ( 
-		select s3.fecha_llegada
-		from servicio s3, tiene t3	
-		where s3.id_servicio = t3.id_servicio 
-		and t3.nro_reserva = r1.nro_reserva)
+	and s.llegada IN ( 
+		select s3.llegada
+		from Servicio s3, Tiene t3	
+		where s3.id_servicio = t3.id_serv 
+		and t3.id_res = r1.id_reserva)
 
 /* Iguales aeropuerto */
-	and v.aeropuerto_salida IN ( 
-		select v4.aeropuerto_salida
-		from servicio s4, tiene t4, vuelvo v4
-		where s4.id_servicio = t4.id_servicio 
-		and t4.nro_reserva = r1.nro_reserva
+	and v.a_salida IN ( 
+		select v4.a_salida
+		from Servicio s4, Tiene t4, Vuelo v4
+		where s4.id_servicio = t4.id_serv 
+		and t4.id_res = r1.id_reserva
 		and s4.id_vuelo = v4.id_vuelo)
 
-	and v.aeropuerto_llegada IN ( 
-		select v5.aeropuerto_llegada
-		from servicio s5, tiene t5, vuelvo v5
-		where s5.id_servicio = t5.id_servicio 
-		and t5.nro_reserva = r1.nro_reserva
+	and v.a_llegada IN ( 
+		select v5.a_llegada
+		from Servicio s5, Tiene t5, Vuelo v5
+		where s5.id_servicio = t5.id_serv
+		and t5.id_res = r1.id_reserva
 		and s5.id_vuelo = v5.id_vuelo)		
 )
