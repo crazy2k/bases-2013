62796d049930b817d7ff3ed1120d5b9c4a0fb687
Vanesa Stricker <vstricker@monits.com>
2013-07-19 17:45:45 -0300

    Ahora si arregle la consulta 2: Antes no contaba bien la cantidad de pasajeron ni distinguia bien los aeropuertos. Basandome en la idea de como estaba hecha la rehice, y probe agregando muchos vuelos y reservas, etc, parece funcionar. Esta es la version entregable.

--- tp1/consultas/2-Viajes.sql
+++ tp1/consultas/2-Viajes.sql
@@ -1,72 +1,11 @@
-SELECT 	(s.cant + l.cant) AS cant_total, 
-			s.cant, 
-			l.cant, 
-			date_format(s.salida, '%m %y') AS salida, 
-			date_format(l.llegada, '%m %y') AS llegada,
-			s.id_a,
-			l.id_a
-			
-	FROM 	(
-			SELECT 	COUNT(*) AS cant, 
-					salida, 
-					id_a 
-
-			FROM	(
-						SELECT 	u.id_user,
-								s.salida,
-								s.llegada,
-								a.id_a
-
-						FROM 	Aeropuerto a,
-								Vuelo v,
-								Servicio s,
-								Tiene t,
-								Reserva r,
-								Hace h,
-								Usuario u
-
-						WHERE v.a_salida = a.id_a
-						AND s.id_vuelo = v.id_vuelo
-						AND t.id_serv = s.id_servicio
-						AND r.id_reserva = t.id_res
-						AND h.id_reserva = r.id_reserva
-						AND u.id_user = h.id_user
-					) us_por_a
-
-			GROUP BY MONTH(salida), YEAR(salida), id_a
-			HAVING 	date_format(salida, '%m %y') between '06 13' AND '08 13'
-			)  s,
-
-			(
-			SELECT 	COUNT(*) AS cant, 
-					llegada, 
-					id_a 
-
-			FROM	(
-						SELECT 	u.id_user,
-								s.salida,
-								s.llegada,
-								a.id_a
-
-						FROM 	Aeropuerto a,
-								Vuelo v,
-								Servicio s,
-								Tiene t,
-								Reserva r,
-								Hace h,
-								Usuario u
-
-						WHERE v.a_salida = a.id_a
-						AND s.id_vuelo = v.id_vuelo
-						AND t.id_serv = s.id_servicio
-						AND r.id_reserva = t.id_res
-						AND h.id_reserva = r.id_reserva
-						AND u.id_user = h.id_user
-					) us_por_a
-
-			GROUP BY MONTH(llegada), YEAR(llegada), id_a
-			HAVING 	date_format(llegada, '%m %y') between '06 13' AND '08 13'
-			)  l
-
-
-ORDER BY cant_total
+SELECT	COUNT(u.id_user) AS cant_pasajeros, DATE_FORMAT(s.salida,'%m %y') AS salida, DATE_FORMAT(s.llegada,'%m %y') AS llegada, a.nombre AS aeropuerto
+FROM Aeropuerto a, Usuario u, Servicio s, Tiene t, Reserva r, Vuelo v, Hace h
+WHERE (a.id_a = v.a_salida OR a.id_a = v.a_llegada)
+	AND s.id_vuelo = v.id_vuelo
+	AND t.id_serv = s.id_servicio
+	AND r.id_reserva = t.id_res
+	AND h.id_reserva = r.id_reserva
+	AND u.id_user = h.id_user
+GROUP BY a.id_a,MONTH(s.salida), YEAR(s.salida), MONTH(s.llegada), YEAR(s.llegada)
+HAVING (salida BETWEEN '06 13' AND '08 13') AND (llegada BETWEEN '06 13' AND '08 13')
+ORDER BY cant_pasajeros
