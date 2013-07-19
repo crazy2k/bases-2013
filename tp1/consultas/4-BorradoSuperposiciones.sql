d259815505baaceb65c4d3be035bd491bae0299b
Vanesa Stricker <vstricker@monits.com>
2013-07-19 16:33:46 -0300

    Arreglo la consulta 4:
    De las reservas que se permite que se superpongan en el tiempo, borrar la mas economica. Ahora si hace eso.

--- tp1/consultas/4-BorradoSuperposiciones.sql
+++ tp1/consultas/4-BorradoSuperposiciones.sql
@@ -1,43 +1,24 @@
-/*Cambiar el select* por delete, es solo para probar*/
-select * 
+delete
 from Reserva r1
 where r1.id_reserva IN (
-	select r2.id_reserva
-	from Reserva r2, Tiene t, Servicio s, Hace h, Usuario u, Vuelo v, Brinda b
-	where t.id_res = r2.id_reserva
-	and t.id_serv = s.id_servicio
-        and h.id_reserva = r2.id_reserva
-        and h.id_user = u.id_user
-        and s.id_vuelo = v.id_vuelo     
-/* Hasta aca tenemos las reservas del usuario */
-        
-/* Iguales fechas */
-        and s.salida IN (
-                select s2.salida
-                from Servicio s2, Tiene t2      
-                where s2.id_servicio = t2.id_serv 
-                and t2.id_res = r1.id_reserva)
-
-        and s.llegada IN ( 
-                select s3.llegada
-                from Servicio s3, Tiene t3      
-                where s3.id_servicio = t3.id_serv 
-                and t3.id_res = r1.id_reserva)
-
-/* Iguales aeropuerto */
-        and v.a_salida IN ( 
-                select v4.a_salida
-                from Servicio s4, Tiene t4, Vuelo v4
-                where s4.id_servicio = t4.id_serv 
-                and t4.id_res = r1.id_reserva
-                and s4.id_vuelo = v4.id_vuelo)
-
-	and v.a_llegada IN ( 
-		select v5.a_llegada
-		from Servicio s5, Tiene t5, Vuelo v5
-		where s5.id_servicio = t5.id_serv
-		and t5.id_res = r1.id_reserva
-		and s5.id_vuelo = v5.id_vuelo)
+	select r3.id_reserva
+	from Reserva r3, Hace h3, Tiene t3, Servicio s3, Usuario u3, Vuelo v3, Brinda b
+	where r3.id_reserva = h3.id_reserva
+		and r3.id_reserva = t3.id_res
+		and u3.id_user = h3.id_user
+		and t3.id_serv = s3.id_servicio
+		and s3.id_vuelo = v3.id_vuelo
+		and s3.salida IN (
+			select s4.salida
+			from Servicio s4, Hace h4, Tiene t4, Reserva r4, Vuelo v4
+			where r4.id_reserva <> r3.id_reserva
+				and h4.id_reserva = r4.id_reserva
+				and h4.id_user = h3.id_user
+				and r4.id_reserva = t4.id_res
+				and t4.id_serv = s4.id_servicio
+				and s4.id_vuelo = v4.id_vuelo
+				and v4.a_salida = v3.a_salida
+				and v4.a_llegada = v4.a_llegada
+				and datediff(s4.salida, now()) > 7)
 	group by b.precio
-	having min(b.precio)
-)
+	having min(b.precio))
